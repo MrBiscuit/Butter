7d18abe3b2dd078af37f6686dc58088340996411
Mr.Biscuit <sunshuaiqi@gmail.com>
2020-01-03 22:16:53 +0800

    Butter | Figma Plugin - Butt layers together
    Butt-together layers neatly and instantly.
    Remove white space and overlaps with one shortcut.
    
    Add quick, consistent spacing between objects
    Space and distribute layers evenly in rows or columns. Simpler than the grid or distribute tool. A massive time saver!

--- code.js
+++ code.js
@@ -4,29 +4,109 @@ function absoluteX(layer) {
 function absoluteY(layer) {
     return layer.absoluteTransform[1][2];
 }
-figma.showUI(__html__, { width: 240, height: 128 });
 let selection = figma.currentPage.selection;
 if (selection.length == 0) {
     figma.closePlugin('No object selected.');
 }
 if (selection.length < 2) {
-    figma.closePlugin('Must select more than one object to run Paddy.');
+    figma.closePlugin('Must select more than one object to run Butter.');
 }
 switch (figma.command) {
     case "up": {
+        figma.showUI(__html__, { width: 240, height: 128 });
         figma.ui.postMessage("Spacing Up:");
         break;
     }
     case "down": {
+        figma.showUI(__html__, { width: 240, height: 128 });
         figma.ui.postMessage("Spaceing Down:");
         break;
     }
     case "left": {
+        figma.showUI(__html__, { width: 240, height: 128 });
         figma.ui.postMessage("Spacing Left:");
         break;
     }
     case "right": {
+        figma.showUI(__html__, { width: 240, height: 128 });
         figma.ui.postMessage("Spaceing Right:");
+        break;
+    }
+    case "bup": {
+        var space = 0;
+        var spaceApplied = 0;
+        var tempXY = 0;
+        var newarr = Array.from(selection).sort((a, b) => absoluteY(a) - absoluteY(b));
+        newarr.forEach(element => {
+            if (spaceApplied == 1) {
+                var realObj = figma.getNodeById(element.id);
+                realObj.y += tempXY - absoluteY(realObj);
+                tempXY = tempXY + space + element.height;
+            }
+            else {
+                tempXY = absoluteY(newarr[0]) + newarr[0].height + space;
+                spaceApplied = 1;
+            }
+        });
+        figma.closePlugin("Butted " + selection.length + " layers");
+        break;
+    }
+    case "bdown": {
+        var space = 0;
+        var spaceApplied = 0;
+        var tempXY = 0;
+        var newarr = Array.from(selection).sort((a, b) => absoluteY(b) - absoluteY(a));
+        newarr.forEach((element, index) => {
+            if (spaceApplied == 1) {
+                var realObj = figma.getNodeById(element.id);
+                realObj.y += tempXY - absoluteY(realObj);
+                tempXY = tempXY - space - (typeof newarr[index + 1] === "undefined" ? 0 : newarr[index + 1].height);
+            }
+            else {
+                tempXY = absoluteY(newarr[0]) - space - newarr[1].height;
+                spaceApplied = 1;
+            }
+        });
+        figma.closePlugin("Butted " + selection.length + " layers");
+        break;
+    }
+    case "bleft": {
+        var space = 0;
+        var spaceApplied = 0;
+        var tempXY = 0;
+        var newarr = Array.from(selection).sort((a, b) => absoluteX(a) - absoluteX(b));
+        newarr.forEach(element => {
+            if (spaceApplied == 1) {
+                var realObj = figma.getNodeById(element.id);
+                realObj.x += tempXY - absoluteX(realObj);
+                tempXY = tempXY + space + element.width;
+            }
+            else {
+                tempXY = absoluteX(newarr[0]) + newarr[0].width + space;
+                spaceApplied = 1;
+            }
+        });
+        figma.closePlugin("Butted " + selection.length + " layers");
+        break;
+    }
+    case "bright": {
+        var space = 0;
+        var spaceApplied = 0;
+        var tempXY = 0;
+        var newarr = Array.from(selection).sort((a, b) => absoluteX(b) - absoluteX(a));
+        newarr.forEach((element, index) => {
+            console.log(absoluteX(element));
+            if (spaceApplied == 1) {
+                var realObj = figma.getNodeById(element.id);
+                realObj.x += tempXY - absoluteX(realObj);
+                tempXY = tempXY - space - (typeof newarr[index + 1] === "undefined" ? 0 : newarr[index + 1].width);
+            }
+            else {
+                tempXY = absoluteX(newarr[0]) - space - newarr[1].width;
+                spaceApplied = 1;
+            }
+        });
+        figma.closePlugin("Butted " + selection.length + " layers");
         break;
     }
 }
@@ -52,6 +132,7 @@ figma.ui.onmessage = (message) => {
                         spaceApplied = 1;
                     }
                 });
+                figma.closePlugin();
                 break;
             }
             case "down": {
@@ -67,6 +148,7 @@ figma.ui.onmessage = (message) => {
                         spaceApplied = 1;
                     }
                 });
+                figma.closePlugin();
                 break;
             }
             case "left": {
@@ -82,6 +164,7 @@ figma.ui.onmessage = (message) => {
                         spaceApplied = 1;
                     }
                 });
+                figma.closePlugin();
                 break;
             }
             case "right": {
@@ -98,6 +181,7 @@ figma.ui.onmessage = (message) => {
                         spaceApplied = 1;
                     }
                 });
+                figma.closePlugin();
                 break;
             }
         }
--- code.ts
+++ code.ts
@@ -6,28 +6,120 @@ function absoluteY(layer) {
     return layer.absoluteTransform[1][2]
 }
 
-figma.showUI(__html__, { width: 240, height: 128 })
-
 let selection = figma.currentPage.selection;
 if (selection.length == 0) {
     figma.closePlugin('No object selected.')
 }
 if (selection.length < 2) {
-    figma.closePlugin('Must select more than one object to run Paddy.')
+    figma.closePlugin('Must select more than one object to run Butter.')
 }
 
 switch (figma.command) {
     case "up": {
+        figma.showUI(__html__, { width: 240, height: 128 })
         figma.ui.postMessage("Spacing Up:")
         break;
     } case "down": {
+        figma.showUI(__html__, { width: 240, height: 128 })
         figma.ui.postMessage("Spaceing Down:")
         break;
     } case "left": {
+        figma.showUI(__html__, { width: 240, height: 128 })
         figma.ui.postMessage("Spacing Left:")
         break;
     } case "right": {
+        figma.showUI(__html__, { width: 240, height: 128 })
         figma.ui.postMessage("Spaceing Right:")
+        break;
+    } case "bup": {
+        var space = 0;
+        var spaceApplied = 0;
+        var tempXY = 0;
+        var newarr = Array.from(selection).sort((a, b) => absoluteY(a) - absoluteY(b));
+
+                newarr.forEach(element => {
+
+                    if (spaceApplied == 1) {
+
+                        var realObj = figma.getNodeById(element.id);
+                        realObj.y += tempXY - absoluteY(realObj);
+                        tempXY = tempXY + space + element.height;
+
+                    } else {
+
+                        tempXY = absoluteY(newarr[0]) + newarr[0].height + space;
+                        spaceApplied = 1;
+                    }
+                });
+                figma.closePlugin("Butted "+ selection.length + " layers");
+                break;
+    } case "bdown": {
+        var space = 0;
+        var spaceApplied = 0;
+        var tempXY = 0;
+        var newarr = Array.from(selection).sort((a, b) => absoluteY(b) - absoluteY(a));
+
+                newarr.forEach((element, index) => {
+
+                    if (spaceApplied == 1) {
+
+                        var realObj = figma.getNodeById(element.id);
+                        realObj.y += tempXY - absoluteY(realObj);
+                        tempXY = tempXY - space - (typeof newarr[index+1] === "undefined" ? 0 : newarr[index+1].height);
+
+                    } else {
+
+                        tempXY = absoluteY(newarr[0]) - space - newarr[1].height;
+                        spaceApplied = 1;
+                    }
+                });
+                figma.closePlugin("Butted "+ selection.length + " layers");
+                break;
+    } case "bleft": {
+        var space = 0;
+        var spaceApplied = 0;
+        var tempXY = 0;
+        var newarr = Array.from(selection).sort((a, b) => absoluteX(a) - absoluteX(b));
+
+                newarr.forEach(element => {
+
+                    if (spaceApplied == 1) {
+
+                        var realObj = figma.getNodeById(element.id);
+                        realObj.x += tempXY - absoluteX(realObj);
+                        tempXY = tempXY + space + element.width;
+
+                    } else {
+
+                        tempXY = absoluteX(newarr[0]) + newarr[0].width + space;
+                        spaceApplied = 1;
+                    }
+                });
+                figma.closePlugin("Butted "+ selection.length + " layers");
+                break;
+    } case "bright": {
+        var space = 0;
+        var spaceApplied = 0;
+        var tempXY = 0;
+        var newarr = Array.from(selection).sort((a, b) => absoluteX(b) - absoluteX(a));
+
+        newarr.forEach((element, index) => {
+            console.log(absoluteX(element));
+
+            if (spaceApplied == 1) {
+
+                var realObj = figma.getNodeById(element.id);
+                realObj.x += tempXY - absoluteX(realObj);
+                tempXY = tempXY - space - (typeof newarr[index+1] === "undefined" ? 0 : newarr[index+1].width);
+
+            } else {
+
+                tempXY = absoluteX(newarr[0]) - space - newarr[1].width;
+                spaceApplied = 1;
+            }
+        });
+        figma.closePlugin("Butted "+ selection.length + " layers");
+
         break;
     }
 }
@@ -65,7 +157,7 @@ figma.ui.onmessage = (message) => {
                         spaceApplied = 1;
                     }
                 });
-
+                figma.closePlugin();
                 break;
 
             } case "down": {
@@ -86,6 +178,8 @@ figma.ui.onmessage = (message) => {
                         spaceApplied = 1;
                     }
                 });
+                figma.closePlugin();
+
                 break;
 
             } case "left": {
@@ -106,6 +200,7 @@ figma.ui.onmessage = (message) => {
                         spaceApplied = 1;
                     }
                 });
+                figma.closePlugin();
 
                 break;
 
@@ -128,6 +223,8 @@ figma.ui.onmessage = (message) => {
                         spaceApplied = 1;
                     }
                 });
+                figma.closePlugin();
+
                 break;
             }
         }
